a1ab067c428c2252916303ba147cb8d6cde56bf3
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-07-27 16:09:12 -0400

    next

--- new.js
+++ new.js
@@ -10,3 +10,31 @@ for (i = 0; i < x; i++) {
 console.log('🍕 🍕 🍕 🍕 🍕 🍕 🍕 🍕 🍕 🍕')
 }
 
+let x = 10;
+let i;
+for (i = 0; i < x; i++) {
+console.log('😶 😐 😑 😬 😯 😮 😲 😊 🙂')
+}
+
+
+let triangles = ['🔼', '🔼🔼', '🔼🔼🔼', '🔼🔼🔼🔼', '🔼🔼🔼🔼🔼', '🔼🔼🔼🔼🔼🔼', '🔼🔼🔼🔼🔼🔼🔼', '🔼🔼🔼🔼🔼🔼🔼🔼']
+for (let index = 0; index < triangles.length; index++) {
+console.log(triangles[index]) 
+}
+
+var triangles = '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
+console.log(triangles)
+var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
+console.log(triangles)
