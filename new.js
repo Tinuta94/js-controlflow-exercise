46063bc070069e0945610ba5c8210d44f7bf0462
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-07-27 17:13:54 -0400

    show the others

--- new.js
+++ new.js
@@ -38,3 +38,34 @@ var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
 console.log(triangles)
 var triangles = '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼' + '🔼'
 console.log(triangles)
+
+
+let v = ['VVVVVVV', 'VVVVVV', 'VVVVV', 'VVVV', 'VVV', 'VV', 'V']
+for (let index = 0; index < v.length; index++) {
+console.log(v[index]) 
+}
+
+
+
+let a = '\xa0\xa0\xa0\xa0\xa0\xa0' + '^' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'
+console.log(a)
+let b = '\xa0\xa0\xa0\xa0\xa0' + '^^^' + '\xa0\xa0\xa0\xa0\xa0'
+console.log(b)
+let c = '\xa0\xa0\xa0\xa0' + '^^^^^' + '\xa0\xa0\xa0\xa0'
+console.log(c)
+let d = '\xa0\xa0\xa0' + '^^^^^^^' + '\xa0\xa0\xa0'
+console.log(d)
+let e = '\xa0\xa0' + '^^^^^^^^^' + '\xa0\xa0'
+console.log(e)
+let f = '\xa0' + '^^^^^^^^^^^' + '\xa0'
+console.log(f)
+
+
+let a = '\xa0' + 'WWWWWWW' + '\xa0'
+console.log(a)
+let b = '\xa0\xa0' + 'WWWWW' + '\xa0\xa0'
+console.log(b)
+let c = '\xa0\xa0\xa0' + 'WWW' + '\xa0\xa0\xa0'
+console.log(c)
+let d = '\xa0\xa0\xa0\xa0' + 'W' + '\xa0\xa0\xa0\xa0'
+console.log(d)
